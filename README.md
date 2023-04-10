c375285e3948f5940a80c6b5703e33f0b7ea8a61
SahayanKY <a1c4f0x324@gmail.com>
2023-04-10 10:45:40 +0900

    feat: pythonの実行環境を確認するコードをモジュールとして分離

---
+++ README.md
@@ -0,0 +1,14 @@
+# PythonEnvChecker
+主要なpythonのライブラリや実行環境に関してチェックを行う。
+
+jupyter notebook等への使用を主に想定。
+
+# ToUse
+1. PYTHONPATHへこのディレクトリを追加
+2. 使う際に以下のpythonコードを追加
+
+```
+import envchecker
+envchecker.check()
+```
+
---
+++ envchecker.py
@@ -0,0 +1,86 @@
+def check():
+    """
+    実行環境をチェック
+    """
+    import socket
+    import platform
+    import datetime
+
+
+    # コンピュータ名を取得
+    print('hostname:{}'.format(socket.gethostname()))
+    # pythonや各ライブラリのバージョンを確認
+    print('python:{}'.format(platform.python_version()))
+    # 実行日時を取得
+    print('current time:{}'.format(datetime.datetime.now()))
+
+    print('library version:')
+
+    libdict = {}
+    # numpy
+    try:
+        import numpy as np
+        libdict['numpy'] = np.__version__
+    except ModuleNotFoundError:
+        pass
+
+    # pandas
+    try:
+        import pandas as pd
+        libdict['pandas'] = pd.__version__
+    except ModuleNotFoundError:
+        pass
+
+    # matplotlib
+    try:
+        import matplotlib
+        libdict['matplotlib'] = matplotlib.__version__
+    except ModuleNotFoundError:
+        pass
+
+    # seaborn
+    try:
+        import seaborn as sns
+        libdict['seaborn'] = sns.__version__
+    except ModuleNotFoundError:
+        pass
+
+    # scipy
+    try:
+        import scipy
+        libdict['scipy'] = scipy.__version__
+    except ModuleNotFoundError:
+        pass
+
+    # sklearn
+    try:
+        import sklearn
+        libdict['sklearn'] = sklearn.__version__
+    except ModuleNotFoundError:
+        pass
+
+    # pytorch
+    try:
+        import torch
+        libdict['torch'] = torch.__version__
+    except ModuleNotFoundError:
+        pass
+
+    # plotly
+    try:
+        import plotly
+        libdict['plotly'] = plotly.__version__
+    except ModuleNotFoundError:
+        pass
+
+    # rdkit
+    try:
+        import rdkit
+        libdict['rdkit'] = rdkit.__version__
+    except ModuleNotFoundError:
+        pass
+
+    for name, version in libdict.items():
+        print('\t{}:{}'.format(name, version))
+
+
